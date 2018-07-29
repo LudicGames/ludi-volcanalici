55c9a7330ad1cb66e93a35044945304143bc6154
Derek Arner <doman412@gmail.com>
2018-07-28 19:16:09 -0700

    update to allow box2d wasm import

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,10 @@ Vue.config.productionTip = false
 
 Vue.use(LudicVue)
 
+// import Box2D from '@ludic/box2d/build/Box2D_v2.3.1_min.wasm.js'
+// import Box2DWasm from 'file-loader!@ludic/box2d/build/Box2D_v2.3.1_min.wasm.wasm'
+// console.log({Box2D, Box2DWasm})
+
 Box2D.then(() => {
   new Vue({
     router,
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,12 @@ const path = require('path')
 
 module.exports = {
   configureWebpack: {
+    module: {
+      defaultRules: [
+        // needed to load the wasm files properly
+        { type: "javascript/auto", resolve: {} },
+      ],
+    },
     resolve: {
       alias: {
         'game': '@/game',
@@ -9,8 +15,8 @@ module.exports = {
         '$entities': '@/game/components/entities',
         '$systems': '@/game/components/systems',
         // allow for easier import syntax
-        'ludic$': '@ludic/ludic/dist/ludic.commonjs2.js',
         '@ludic/ludic$': '@ludic/ludic/dist/ludic.commonjs2.js',
+        'ludic$': '@ludic/ludic',
         'ludic-vue$': '@ludic/ludic-vue',
         'ludic-box2d$': '@ludic/ludic-box2d',
         'ein$': '@ludic/ein',
